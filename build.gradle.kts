c8130a019799b43b860c2a501564cd17d9de055e
Shionic <shionic@yandex.com>
2024-08-10 22:40:59 +0700

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.1.4"
-	id("io.spring.dependency-management") version "1.1.3"
-	id("io.freefair.lombok") version "8.4"
+	id("org.springframework.boot") version "3.3.2"
+	id("io.spring.dependency-management") version "1.1.6"
+	id("io.freefair.lombok") version "8.7.1"
 }
 
 group = "simplesso"
-version = "0.0.1-SNAPSHOT"
+version = "1.0.0-SNAPSHOT"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
@@ -20,13 +20,13 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-authorization-server")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.google.crypto.tink:tink:1.9.0")
-	implementation("org.slf4j:slf4j-api:2.0.7")
-	implementation("org.slf4j:jcl-over-slf4j:2.0.7")
+	implementation("com.google.crypto.tink:tink:1.14.1")
+	implementation("org.slf4j:slf4j-api:2.0.15")
+	implementation("org.slf4j:jcl-over-slf4j:2.0.15")
 	runtimeOnly("org.postgresql:postgresql")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	runtimeOnly("com.h2database:h2:2.2.220")
+	runtimeOnly("com.h2database:h2:2.3.230")
 }
 
 tasks.withType<Test> {
